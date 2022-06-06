8b92f4b2668f6f8d21b29992c3bced6f97942833
XoLoDiLniC <99400502+XoLoDiLniC@users.noreply.github.com>
2022-06-06 09:44:11 +0300

    XoLoDiLnIk

---
+++ Курсовой проект/RCD/RCD/Orders.xaml.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace RCD
+{
+    /// <summary>
+    /// Interaction logic for Orders.xaml
+    /// </summary>
+    public partial class Orders : Page
+    {
+        private int idus;
+        private Order _currentorder = new Order();
+        public Orders(Order selectedOrder, int id)
+        {
+            idus = id;
+            InitializeComponent();
+            if (selectedOrder != null)
+            {
+                _currentorder = selectedOrder;
+            }
+            DataContext = _currentorder;
+
+        }
+
+        private void btnLog_click(object sender, RoutedEventArgs e)
+        {
+            if (_currentorder.Id_Order == 0)
+            {
+                Entities1.GetContext().Orders.Add(new Order {Where1 = Where1Tb.Text , Where2 = Where2Tb.Text , id_user = idus ,TypeDil =TdeliveryTb.Text, TypePay = TpaymentTb.Text, What = WhatSendTb.Text});
+            }
+            Entities1.GetContext().SaveChanges();
+            MessageBox.Show("Спасибо, что выбрали нашу доставку!!!");
+            Manager.MainFrame.Navigate(new User(idus));
+        }
+    }
+}
---
+++ Курсовой проект/RCD/RCD/Reg.xaml.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace RCD
+{
+    /// <summary>
+    /// Interaction logic for Reg.xaml
+    /// </summary>
+    public partial class Reg : Window
+    {
+        private Userss _currentUser = new Userss();
+        public Reg()
+        {
+            DataContext = _currentUser;
+            InitializeComponent();
+        }
+
+        private void btnLog_click(object sender, RoutedEventArgs e)
+        {
+            bool canRegister = Entities1.GetContext().Usersses.All(p => p.Login != LoginTb.Text.Trim());
+            if (canRegister)
+            {
+                Entities1.GetContext().Usersses.Add(_currentUser);
+                Entities1.GetContext().SaveChanges();
+                MessageBox.Show("Вы прошли регистрацию");
+                Login login = new Login();
+                login.Show();
+                Close();
+            }
+            else
+            {
+                MessageBox.Show("Вы не прошли регистрацию, такой Email существует");
+            }
+        }
+    }
+}
